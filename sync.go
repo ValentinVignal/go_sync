ef961e713af53512a7ec51900e3d53314e353b9b
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-06-29 14:00:00 +0100

    :sparkles: Sync forms

--- sync.go
+++ sync.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 )
 
 type SyncedProject struct {
@@ -15,8 +16,17 @@ type SyncedProject struct {
 	Code string `json:"code"`
 }
 
+type SyncedForm struct {
+	ID        string                 `json:"id"`
+	ProjectID string                 `json:"projectID"`
+	Name      string                 `json:"name"`
+	Data      map[string]interface{} `json:"data"`
+	TaskIDs   []string               `json:"taskIDs"`
+}
+
 type SyncedData struct {
 	Projects []SyncedProject `json:"projects"`
+	Forms    []SyncedForm    `json:"forms"`
 }
 
 func Sync(c *gin.Context) {
@@ -29,21 +39,21 @@ func Sync(c *gin.Context) {
 	}
 	defer database.Close()
 
-	rows, err := database.Query("SELECT id, name, code FROM project")
+	projectRows, err := database.Query(`SELECT id, name, code FROM project  WHERE "deletedAt" IS NULL ORDER BY "updatedAt" DESC LIMIT 1000`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
+	defer projectRows.Close()
 
 	projects := []SyncedProject{}
-	for rows.Next() {
+	for projectRows.Next() {
 		var (
 			id   string
 			name string
 			code string
 		)
 
-		if err := rows.Scan(&id, &name, &code); err != nil {
+		if err := projectRows.Scan(&id, &name, &code); err != nil {
 			log.Fatal(err)
 		} else {
 			projects = append(projects, SyncedProject{
@@ -54,8 +64,44 @@ func Sync(c *gin.Context) {
 		}
 	}
 
+	formsRows, err := database.Query(`SELECT form.id, form.name, form."projectID" as project_id, form.data, array_remove(array_agg(forms_tasks."taskID"), NULL) as task_ids FROM form
+        LEFT JOIN forms_tasks ON form.id = forms_tasks."formID"
+        WHERE form."deletedAt" IS NULL
+        GROUP BY form.id
+        ORDER BY form."updatedAt" DESC LIMIT 1000;`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer formsRows.Close()
+
+	forms := []SyncedForm{}
+	for formsRows.Next() {
+		var (
+			id        string
+			name      string
+			projectID string
+			data      []byte
+			taskIDs   []string
+		)
+
+		if err := formsRows.Scan(&id, &name, &projectID, &data, pq.Array(&taskIDs)); err != nil {
+			log.Fatal(err)
+		} else {
+			unMarshaledData := map[string]interface{}{}
+			json.Unmarshal(data, &unMarshaledData)
+			forms = append(forms, SyncedForm{
+				ID:        id,
+				Name:      name,
+				ProjectID: projectID,
+				Data:      unMarshaledData,
+				TaskIDs:   taskIDs,
+			})
+		}
+	}
+
 	syncedData := SyncedData{
 		Projects: projects,
+		Forms:    forms,
 	}
 
 	c.JSON(http.StatusOK, syncedData)
